13f4f086dedd3b0ca2a72ccd267865a29965813c
lcomstive <lcomstive@hotmail.com>
2023-07-15 09:20:40 +0000

    Fixed top-right navbar element placing

--- controllers/search.js
+++ controllers/search.js
@@ -3,7 +3,7 @@ const Post = require('../database/models/Post')
 module.exports = async (req, res) =>
 {
 	let posts = await Post.find({})
-	res.render('allPosts', {
+	res.render('searchResults', {
 		posts,
 		searchQuery: req.params.query,
 		auth: req.session.renderer,
